5d4f73dd7575c64950e224ccaaf49c9e2732dd15
ehenon <emilienhenon@gmail.com>
2022-12-14 15:03:52 +0100

    feat: switch the repo to full ts ✨

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+import type { Config } from 'jest';
+
+export default async (): Promise<Config> => ({
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: [
@@ -19,4 +20,4 @@ module.exports = {
     './.+/build',
   ],
   testRegex: '.*\\.spec\\.ts$',
-};
+});
